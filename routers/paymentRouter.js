78d1d4c7359a0a4d85815d927b72d5bbcd876520
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-06-13 04:19:58 -0600

    Add files via upload

---
+++ routers/paymentRouter.js
@@ -0,0 +1,16 @@
+const { initPayment, ipn, paymentSuccess, paymentCancel, paymentFail } = require('../controllers/paymentController');
+const authorize = require('../middlewares/authorize');
+const router = require('express').Router();
+
+router.route('/')
+    .get(authorize, initPayment);
+
+router.post('/ipn');
+
+router.post('/success', paymentSuccess);
+
+router.post('/cancel', paymentCancel);
+
+router.post('/fail', paymentFail);
+
+module.exports = router;
